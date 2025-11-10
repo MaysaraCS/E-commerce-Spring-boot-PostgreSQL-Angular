a1736ab92bee5cc2ae3275ccfec19457e8cd0d03
Maysara <maysaracs1001@gmail.com>
2025-11-10 20:04:50 +0300

    order package added secondary

---
+++ ecommerce/apps/ecom-backend/src/main/java/com/ecommerceBackend/ecom/order/infrastructure/secondary/repository/JpaOrderedProductRepository.java
@@ -0,0 +1,10 @@
+package com.ecommerceBackend.ecom.order.infrastructure.secondary.repository;
+
+
+import com.ecommerceBackend.ecom.order.infrastructure.secondary.entity.OrderedProductEntity;
+import com.ecommerceBackend.ecom.order.infrastructure.secondary.entity.OrderedProductEntityPk;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaOrderedProductRepository extends JpaRepository<OrderedProductEntity, OrderedProductEntityPk> {
+
+}

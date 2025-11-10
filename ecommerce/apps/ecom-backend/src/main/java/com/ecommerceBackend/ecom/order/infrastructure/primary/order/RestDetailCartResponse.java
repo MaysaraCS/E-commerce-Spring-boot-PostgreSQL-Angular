3e15f893f36d5ef9c1787b5fc3950537df17abe0
Maysara <maysaracs1001@gmail.com>
2025-11-10 19:54:33 +0300

    order package added primary

---
+++ ecommerce/apps/ecom-backend/src/main/java/com/ecommerceBackend/ecom/order/infrastructure/primary/order/RestDetailCartResponse.java
@@ -0,0 +1,17 @@
+package com.ecommerceBackend.ecom.order.infrastructure.primary.order;
+
+
+import com.ecommerceBackend.ecom.order.domain.order.aggregate.DetailCartResponse;
+import org.jilt.Builder;
+
+import java.util.List;
+
+@Builder
+public record RestDetailCartResponse(List<RestProductCart> products) {
+
+  public static RestDetailCartResponse from(DetailCartResponse detailCartResponse) {
+    return RestDetailCartResponseBuilder.restDetailCartResponse()
+      .products(detailCartResponse.getProducts().stream().map(RestProductCart::from).toList())
+      .build();
+  }
+}
---
+++ ecommerce/apps/ecom-backend/src/main/java/com/ecommerceBackend/ecom/order/infrastructure/primary/order/RestStripeSession.java
@@ -0,0 +1,16 @@
+package com.ecommerceBackend.ecom.order.infrastructure.primary.order;
+
+
+import com.ecommerceBackend.ecom.order.domain.order.vo.StripeSessionId;
+import org.jilt.Builder;
+
+@Builder
+public record RestStripeSession(String id) {
+
+
+  public static RestStripeSession from(StripeSessionId stripeSessionId) {
+    return RestStripeSessionBuilder.restStripeSession()
+      .id(stripeSessionId.value())
+      .build();
+  }
+}

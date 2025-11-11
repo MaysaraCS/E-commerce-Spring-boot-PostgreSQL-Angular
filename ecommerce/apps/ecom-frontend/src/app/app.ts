9e914e0abd9d1c4947c0151bcfd3bf2660ce24b5
Maysara <maysaracs1001@gmail.com>
2025-11-11 12:23:06 +0300

    auth added

--- ecommerce/apps/ecom-frontend/src/app/app.ts
+++ ecommerce/apps/ecom-frontend/src/app/app.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import { Footer } from './../../layout/footer/footer';
+import { Navbar } from './../../layout/navbar/navbar';
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
+import {
+  FaConfig,
+  FaIconComponent,
+  FaIconLibrary,
+} from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import { Oauth2Service } from './auth/oauth2.service';
+import { isPlatformBrowser, NgClass } from '@angular/common';
+import { ToastService } from './shared/toast/toast.service';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
-  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterModule,
+    FaIconComponent,
+    Navbar,
+    Footer,
+    NgClass,
+  ],
+  selector: 'ecom-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
-  protected title = 'ecom-frontend';
+export class App implements OnInit {
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig);
+
+  private oauth2Service = inject(Oauth2Service);
+
+  toastService = inject(ToastService);
+
+  platformId = inject(PLATFORM_ID);
+
+  constructor() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.oauth2Service.initAuthentication();
+    }
+    this.oauth2Service.connectedUserQuery = this.oauth2Service.fetch();
+  }
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faConfig.defaultPrefix = 'far';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
